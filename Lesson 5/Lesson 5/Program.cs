a11fc0010bc5c1e77e0d72aa8584b48468ca0f5e
kras174 <kras32a@gmail.com>
2016-08-20 16:28:28 +0500

    Добавлен метод проверки сложности пароля

--- Lesson 5/Lesson 5/Program.cs
+++ Lesson 5/Lesson 5/Program.cs
@@ -12,13 +12,37 @@ namespace Lesson_5
         static void Main(string[] args)
         {
             string login = string.Empty;
+            string password = string.Empty;
 
-            Console.WriteLine("Введите логин");
-            login = Console.ReadLine();
-            CheckLogin(login);
+            //Console.WriteLine("Введите логин");
+            //login = Console.ReadLine();
+            //CheckLogin(login);
             //CheckLoginReg(login);
 
+            Console.WriteLine("Введите пароль");
+            password = Console.ReadLine();
+
+            CheckPassword(password);
+
             Console.ReadKey();
+        }
+
+        //-----Метод проверяет сложность пароля
+        private static void CheckPassword(string password)
+        {
+            /* Пароль должен быть от 3х символов и до 10
+             * Пароль первой сложности только буквы или цифры
+             * Пароль второй сложности и буквы и цифры
+             * Пароль третей сложности строчные и заглавные буквы и цифры
+             */
+            Regex level1 = new Regex(@"^[a-z]|[0-9]{3,9}$");
+            Regex level2 = new Regex(@"^[a-z0-9]{3,9}$");
+            Regex level3 = new Regex(@"^[a-zA-Z0-9]{3,9}$");
+
+            if (level1.IsMatch(password)) Console.WriteLine("Уровень пароля 1");
+            else if (level2.IsMatch(password)) Console.WriteLine("Уровень пароля 2");
+            else if (level3.IsMatch(password)) Console.WriteLine("Уровень пароля 3");
+
         }
 
         //-----Метод проверяет регуляркой корректность ввода логина. Корректным логином будет строка от 2-х до 10-ти символов, содержащая только буквы и цифры, и при этом цифра не может быть первой.
