e8056f302eb1cf225ffdffe90a54984fb152c659
kras174 <kras32a@gmail.com>
2016-08-20 16:07:53 +0500

    Первое задание выполнено

--- Lesson 5/Lesson 5/Program.cs
+++ Lesson 5/Lesson 5/Program.cs
@@ -15,12 +15,13 @@ namespace Lesson_5
 
             Console.WriteLine("Введите логин");
             login = Console.ReadLine();
-            //CheckLogin(login);
-            CheckLoginReg(login);
+            CheckLogin(login);
+            //CheckLoginReg(login);
 
             Console.ReadKey();
         }
 
+        //-----Метод проверяет регуляркой корректность ввода логина. Корректным логином будет строка от 2-х до 10-ти символов, содержащая только буквы и цифры, и при этом цифра не может быть первой.
         private static void CheckLoginReg(string login)
         {
             Regex reg = new Regex(@"^\D+[a-zA-Z0-9]{2,9}$");
@@ -29,6 +30,7 @@ namespace Lesson_5
             else Console.WriteLine("Неверный логин!");
         }
 
+        //-----Метод проверяет корректность ввода логина. Корректным логином будет строка от 2-х до 10-ти символов, содержащая только буквы и цифры, и при этом цифра не может быть первой.
         private static void CheckLogin(string login)
         {
             bool isCorrect = false;
@@ -37,6 +39,7 @@ namespace Lesson_5
             {
                 foreach (char c in login)
                 {
+                    if (char.IsNumber(c)) break;
                     if ((char.IsLetter(c) || char.IsNumber(c)))
                     {
                         isCorrect = true;
