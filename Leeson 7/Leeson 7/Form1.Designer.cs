cbeec1bca344dbb8b44638e7e27ff9e8d3ee0821
kras174 <kras32a@gmail.com>
2016-08-27 16:23:23 +0500

    first commit

---
+++ Leeson 7/Leeson 7/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Leeson_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbDirectoruField = new System.Windows.Forms.TextBox();
+            this.btnChoseDirectory = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbDirectoruField
+            // 
+            this.tbDirectoruField.Location = new System.Drawing.Point(12, 12);
+            this.tbDirectoruField.Name = "tbDirectoruField";
+            this.tbDirectoruField.Size = new System.Drawing.Size(236, 20);
+            this.tbDirectoruField.TabIndex = 0;
+            // 
+            // btnChoseDirectory
+            // 
+            this.btnChoseDirectory.Location = new System.Drawing.Point(253, 11);
+            this.btnChoseDirectory.Name = "btnChoseDirectory";
+            this.btnChoseDirectory.Size = new System.Drawing.Size(27, 23);
+            this.btnChoseDirectory.TabIndex = 1;
+            this.btnChoseDirectory.Text = "button1";
+            this.btnChoseDirectory.UseVisualStyleBackColor = true;
+            this.btnChoseDirectory.Click += new System.EventHandler(this.btnChoseDirectory_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ReadDataFrpmFile";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 163);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnChoseDirectory);
+            this.Controls.Add(this.tbDirectoruField);
+            this.MaximumSize = new System.Drawing.Size(1000, 600);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbDirectoruField;
+        private System.Windows.Forms.Button btnChoseDirectory;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Leeson 7/Leeson 7/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Leeson_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileInfo fi = new FileInfo(@"E:\C# Lessons\N7\files\file.txt");
+
+            string allData = string.Empty;
+
+            if (!fi.Exists)
+                fi.Create();
+
+            using (var fileStream = fi.OpenRead())
+            {
+                byte[] buffer = new byte[2048];
+                int i = 0;
+                int n = 0;
+
+                while ((n = fileStream.ReadByte()) > 0)
+                {
+                    buffer[i] = (byte)n;
+                }
+
+                allData = Encoding.UTF8.GetString(buffer);
+            }
+
+            MessageBox.Show(allData, "Файл прочитан!", MessageBoxButtons.OK);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnChoseDirectory_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
