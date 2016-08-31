befc39c72215e7419a93532c54775487ae8056f9
kras174 <kras32a@gmail.com>
2016-08-31 13:37:41 +0500

    HW2 done!

--- Lesson N3/Lesson N3_HW2/Program.cs
+++ Lesson N3/Lesson N3_HW2/Program.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace Lesson_N3_HW2
 {
+    //2.С клавиатуры вводятся числа, пока не будет введен 0 (каждое число в новой строке). 
+    //Требуется подсчитать сумму всех нечетных положительных чисел.Сами числа и сумму вывести на экран;
+    //Добавить обработку исключительных ситуаций на то, что могут быть введены не корректные данные.При возникновении ошибки вывести сообщение. 
+    //Красильнмиков Антон
+
     class Program
     {
         static void Main(string[] args)
@@ -15,27 +20,29 @@ namespace Lesson_N3_HW2
 
             while (true)
             {
-                Console.WriteLine("Введите любое число или 0 для выхода");
                 try
                 {
+                    Console.WriteLine("Введите любое число или 0 для подсчёта резултата");
                     num = int.Parse(Console.ReadLine());
-                    if (num == 0) break;
-                    if ((num > 0))
+                    if (num == 0)
                     {
-                        result += num;
+                        Console.WriteLine("Сумма всех нечётных положительных чисел равна: {0}", result);
+                        Console.ReadKey();
+                        break;
                     }
+                    if ((num > 0) && (num % 2 != 0))
+                        result += num;
                 }
                 catch (FormatException ex)
                 {
-                    Console​.WriteLine​(​"Не правильно ввели данные"​);                  
-                    ​Console.WriteLine(​ex.Message);
+                    Console​.WriteLine​("Неверный формат данных!");
+                    //Console.WriteLine(ex.Message);
                 }
                 catch
                 {
                     Console.WriteLine("Ошибка ввода! Попробуйте ещё раз!");
                 }
             }
-
         }
     }
 }
