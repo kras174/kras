7c79cecc45a391c204a482afb7e1381fff0f824e
kras174 <kras32a@gmail.com>
2016-08-31 13:24:38 +0500

    first commit

---
+++ Lesson N3/Lesson N3_HW2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_N3_HW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            int result = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Введите любое число или 0 для выхода");
+                try
+                {
+                    num = int.Parse(Console.ReadLine());
+                    if (num == 0) break;
+                    if ((num > 0))
+                    {
+                        result += num;
+                    }
+                }
+                catch (FormatException ex)
+                {
+                    Console​.WriteLine​(​"Не правильно ввели данные"​);                  
+                    ​Console.WriteLine(​ex.Message);
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка ввода! Попробуйте ещё раз!");
+                }
+            }
+
+        }
+    }
+}
